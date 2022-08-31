8123220e1ee263d621d2ac0fb61383dc6f21eed6
kamrul1275 <kamrul1275.info@gmail.com>
2022-08-31 13:27:57 +0600

    post add

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+  public function postView()
+  {
+
+
+    $posts = Post::all();
+   return view('post',compact('posts'));
+  }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PhoneController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',  [PhoneController::class, 'index']);
+
+
+
+Route::get('/post',  [PostController::class, 'postView']);
